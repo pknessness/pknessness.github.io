f4579a3c635e0d37484e75bd3e436d8215fd413d
Anshal Jain <pk.ness.ness@gmail.com>
2024-08-25 16:29:27 -0700

    added font

--- starcraft_randomizer/main.js
+++ starcraft_randomizer/main.js
@@ -28,9 +28,15 @@ function gen(){
             break;
         }
     }
-    document.getElementById("struct1").innerText = name1 + ": "+ s1[r1][0];
-    document.getElementById("struct2").innerText = name2 + ": "+ s2[r2][0];
-    document.getElementById("struct3").innerText = name3 + ": "+ s3[r3][0];
+
+    document.getElementById("struct1_l").innerText = name1;
+    document.getElementById("struct1").innerText = s1[r1][0];
+
+    document.getElementById("struct2_l").innerText = name2;
+    document.getElementById("struct2").innerText = s2[r2][0];
+    
+    document.getElementById("struct3_l").innerText = name3;
+    document.getElementById("struct3").innerText = s3[r3][0];
 
     document.getElementById("reroll_1").innerText = "Reroll "+ s1[r1][0];
     document.getElementById("reroll_2").innerText = "Reroll "+ s2[r2][0];
@@ -79,7 +85,8 @@ function reroll1(){
             break;
         }
     }
-    document.getElementById("struct1").innerText = name1 + ": "+ s1[r1][0];
+    document.getElementById("struct1_l").innerText = name1;
+    document.getElementById("struct1").innerText = s1[r1][0];
     document.getElementById("reroll_1").innerText = "Reroll "+ s1[r1][0];
 }
 
@@ -95,7 +102,8 @@ function reroll2(){
             break;
         }
     }
-    document.getElementById("struct2").innerText = name2 + ": "+ s2[r2][0];
+    document.getElementById("struct2_l").innerText = name1;
+    document.getElementById("struct2").innerText = s1[r1][0];
     document.getElementById("reroll_2").innerText = "Reroll "+ s2[r2][0];
 }
 
@@ -111,6 +119,7 @@ function reroll3(){
             break;
         }
     }
-    document.getElementById("struct3").innerText = name3 + ": "+ s3[r3][0];
+    document.getElementById("struct3_l").innerText = name1;
+    document.getElementById("struct3").innerText = s1[r1][0];
     document.getElementById("reroll_3").innerText = "Reroll "+ s3[r3][0];
 }
